c6585b3e14c5daa4bbb5785d857103e828684917
Pe-te-r <mburupeter8526@gmail.com>
2024-07-23 10:53:48 +0300

    better changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build', // Ensure this is 'dist'
+    outDir: 'dist', // Ensure this is 'dist'
     rollupOptions: {
       output: {
         manualChunks: {
