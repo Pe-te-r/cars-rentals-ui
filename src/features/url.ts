b508a8f33c33df3c4d8822d61ad553ebd5c57d21
Pe-te-r <mburupeter8526@gmail.com>
2024-07-26 21:42:20 +0300

    changes

--- src/features/url.ts
+++ src/features/url.ts
@@ -1,2 +1,2 @@
-// export const urlApi ='https://car-rental-api-tlcz.onrender.com/api/'  
-export const urlApi ='http://localhost:3000/api'  
+export const urlApi ='https://car-rental-api-tlcz.onrender.com/api/'  
+// export const urlApi ='http://localhost:3000/api'  
